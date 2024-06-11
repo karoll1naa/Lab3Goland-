2ef215cb58edf6dad9ced9c3d221a86a158097f9
karoll1naa <karollinnajan@gmail.com>
2024-06-11 16:34:01 +0300

    Added a T shape implementation and set the shape color to blue

--- ui/window.go
+++ ui/window.go
@@ -32,8 +32,8 @@ type Visualizer struct {
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	pw.pos.Max.X = 400
+	pw.pos.Max.Y = 400
 	driver.Main(pw.run)
 }
 
@@ -43,7 +43,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -123,7 +125,6 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 		if t == nil {
 			pw.drawDefaultUI()
 		} else {
-			// Використання текстури отриманої через виклик Update.
 			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
 		}
 		pw.w.Publish()
@@ -132,6 +133,31 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	RectangleOne := image.Rectangle{
+		Min: image.Point{
+			X: pw.pos.Max.X - 150,
+			Y: pw.pos.Max.Y,
+		},
+		Max: image.Point{
+			X: pw.pos.Max.X + 150,
+			Y: pw.pos.Max.Y - 100,
+		},
+	}
+	RectangleColorOne := color.RGBA{B: 255, A: 255}
+	pw.w.Fill(RectangleOne, RectangleColorOne, draw.Src)
+
+	RectangleTwo := image.Rectangle{
+		Min: image.Point{
+			X: pw.pos.Max.X - 50,
+			Y: pw.pos.Max.Y - 100,
+		},
+		Max: image.Point{
+			X: pw.pos.Max.X + 50,
+			Y: pw.pos.Max.Y + 100,
+		},
+	}
+	RectangleColorTwo := color.RGBA{B: 255, A: 255}
+	pw.w.Fill(RectangleTwo, RectangleColorTwo, draw.Src)
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 
